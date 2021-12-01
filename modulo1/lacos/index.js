8d2ca2bab8522d4a6effdb1397433d509966e386
Robson Rodrigues Ribeiro <robson.ribeiro@micropower.com.br>
2021-12-01 08:51:44 -0300

    Aula - laços, removendo comentários

--- modulo1/lacos/index.js
+++ modulo1/lacos/index.js
@@ -1,67 +1,67 @@
-// {
-//     let valor = 0
-//     for(let i = 0; i < 5; i++) {
-//     valor += i
-//     }
-//     console.log(valor) 
-//     //imprime 10
-// }
+{
+    let valor = 0
+    for(let i = 0; i < 5; i++) {
+    valor += i
+    }
+    console.log(valor) 
+    //imprime 10
+}
 
-// {
-//     const lista = [10, 11, 12, 15, 18, 19, 21, 23, 25, 27, 30]
-//     for (let numero of lista) {
-//     if (numero > 18) {
-//             console.log(numero, lista.indexOf(numero))
-//         }
-//     }
-//     //a) O que vai ser impresso no console?
-//     //Impremi do 19 ao 30
-//     //b) Se eu quisesse acessar o índice de cada elemento dessa lista, o for...of... é 
-//     //suficiente? Se sim, o que poderia ser usado para fazer isso?
-//     //indexOf(numero)
-// }
+{
+    const lista = [10, 11, 12, 15, 18, 19, 21, 23, 25, 27, 30]
+    for (let numero of lista) {
+    if (numero > 18) {
+            console.log(numero, lista.indexOf(numero))
+        }
+    }
+    //a) O que vai ser impresso no console?
+    //Impremi do 19 ao 30
+    //b) Se eu quisesse acessar o índice de cada elemento dessa lista, o for...of... é 
+    //suficiente? Se sim, o que poderia ser usado para fazer isso?
+    //indexOf(numero)
+}
 
-// {
-//    debugger
-//     const quantidadeTotal = Number(prompt("Digite a quantidade de linhas: "))
-//     let quantidadeAtual = 0
-//     while(quantidadeAtual < quantidadeTotal){
-//     let linha = ""
-//     for(let asteriscos = 0; asteriscos < quantidadeAtual + 1; asteriscos++){
-//         linha += "*"
-//     }
-//     console.log(linha)
-//     quantidadeAtual++
-//     }
+{
+   debugger
+    const quantidadeTotal = Number(prompt("Digite a quantidade de linhas: "))
+    let quantidadeAtual = 0
+    while(quantidadeAtual < quantidadeTotal){
+    let linha = ""
+    for(let asteriscos = 0; asteriscos < quantidadeAtual + 1; asteriscos++){
+        linha += "*"
+    }
+    console.log(linha)
+    quantidadeAtual++
+    }
 
-// }
+}
 
-// {
-//     /*
-//         1. Pergunte ao usuário quantos bichinhos de estimação ele tem e guarde esse dado em uma variável.
-//         a) Se a quantidade for 0, imprima no console "Que pena! Você pode adotar um pet!"
+{
+    /*
+        1. Pergunte ao usuário quantos bichinhos de estimação ele tem e guarde esse dado em uma variável.
+        a) Se a quantidade for 0, imprima no console "Que pena! Você pode adotar um pet!"
 
-//         b) Se a quantidade for maior que 0, solicite que o usuário digite os nomes deles, um por um, e guarde esses nomes em um array
-//     */
-//   2
-//     const qtdBichinhos = Number(prompt("Informe quantos bicho de estimação vc tem?"))
-//     let meusBichinhos = []
-//     let nomeBichinho = ""
+        b) Se a quantidade for maior que 0, solicite que o usuário digite os nomes deles, um por um, e guarde esses nomes em um array
+    */
+  2
+    const qtdBichinhos = Number(prompt("Informe quantos bicho de estimação vc tem?"))
+    let meusBichinhos = []
+    let nomeBichinho = ""
 
-// if(qtdBichinhos === 0){
-//     imprimir("Que pena! Você pode adotar um pet!")
-// }else{
-//     for(let index = 0; index < qtdBichinhos; index++ ){
-//         nomeBichinho = prompt(`Informe o nome do seu ${index+1}© bichinho`)
-//         meusBichinhos.push(nomeBichinho)
-//     } 
-//     imprimir(meusBichinhos)
-// }    
+if(qtdBichinhos === 0){
+    imprimir("Que pena! Você pode adotar um pet!")
+}else{
+    for(let index = 0; index < qtdBichinhos; index++ ){
+        nomeBichinho = prompt(`Informe o nome do seu ${index+1}© bichinho`)
+        meusBichinhos.push(nomeBichinho)
+    } 
+    imprimir(meusBichinhos)
+}    
     
-//     function imprimir(objeto){
-//         console.log(objeto)
-//     }
-// }
+    function imprimir(objeto){
+        console.log(objeto)
+    }
+}
 
 {
     /*
@@ -114,10 +114,10 @@
         console.log(`Maior número ${maiorNumero}, Menor número ${menorNumero}`)
     }
 
-//  imprimirArrayOriginal(arrayOriginal) //a
-//  imprimirArrayDivididoPor10(arrayOriginal) //b
-//  imprimirArrayValoresPar(arrayOriginal) //c
-//  imprimirNovoArray(arrayOriginal)
+ imprimirArrayOriginal(arrayOriginal) //a
+ imprimirArrayDivididoPor10(arrayOriginal) //b
+ imprimirArrayValoresPar(arrayOriginal) //c
+ imprimirNovoArray(arrayOriginal)
 imprimirMaiorMenoNumeroArray(arrayOriginal)
 
 
