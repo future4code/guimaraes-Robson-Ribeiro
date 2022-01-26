42f4048d2930a239d3bdef9c6747cb88bdf47815
Robson Ribeiro <robsonribeiro@uffa.com.vc>
2022-01-25 22:28:09 -0300

    criado meu projeto Labetube, criado o link do surge

---
+++ modulo1/jsx-labetube/labetube/src/components/Logo.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import { HiOutlinePlay } from "react-icons/hi"
+
+function Logo( ){
+    return(
+        <>
+            <HiOutlinePlay size={40} />
+        </>
+    );
+}
+
+
+export default Logo;
