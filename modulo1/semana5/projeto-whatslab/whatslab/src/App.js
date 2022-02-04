d3ba681eabc0c720d38be751a77cfcc02cc73264
Robson R Ribeiro <projetos.r@hotmail.com>
2022-02-04 15:15:52 -0300

    s

--- modulo1/semana5/projeto-whatslab/whatslab/src/App.js
+++ modulo1/semana5/projeto-whatslab/whatslab/src/App.js
@@ -1,7 +1,3 @@
-import logo from './logo.svg';
-import './App.css';
-import Input from './components/Input/Input';
-import Button from './components/Button/Button'; 
 import React from 'react';
 
 //css
@@ -10,84 +6,15 @@ import * as S from './style'
 
 class App extends React.Component {
 
-  state = {
-    message: '',
-    user: '',
-    typing: [],
-    count: 0
-  }
-
-
-  handleEnviar = () => {
-    const sendMessage = this.state.typing;
-    
-    sendMessage.push({
-      userTyping: this.state.user,
-      messageUserTyping: this.state.message,
-      url: 'https://picsum.photos/50/5' + this.state.count.toString()
-    });
-    this.setState({ count: this.state.count + 1})
-    this.setState({ userTyping: [...this.state.typing, sendMessage ] });
-  }
-
-  handleOnchangeUser = ( event ) => {
-    this.setState({ user: event.target.value })
-  }
-
-  handleOnchangeMessage = ( event ) => {
-    this.setState({ message: event.target.value })
-  }
-
-
+ 
   render(){
       
     return (
-      <S.Container key={0}>
+      <S.WhatsContainer>
         <S.WhatsTyping>
-         {this.state.typing.map( (type, index ) => {
-           return (<>
-                    <S.Container style={{ display: 'inline' }}>  
-                      <S.Photo src={type.url} alt="Icon" />                   
-                      <S.WhatsUserName key={index}>
-                        
-                        {type.userTyping}: 
-
-                      </S.WhatsUserName>
-                      <S.WhatsUserNameTyping>{type.messageUserTyping}</S.WhatsUserNameTyping> 
-                      <hr />
-                    </S.Container>
-            </>
-          )           
-         })}
-        </S.WhatsTyping>
-        <S.WhatsContainer height={'50px'}>
-          <Input 
-            width={'15%'} 
-            height={'30px'} 
-            placeholder={'Usuário'} 
-            value={this.state.user} 
-            onChange={this.handleOnchangeUser}
-          />
-          
-          <Input 
-            width={'66%'} 
-            height={'30px'} 
-            placeholder={'Mensagem'} 
-            value={this.state.message} 
-            onChange={this.handleOnchangeMessage}
-          />     
-
-          <Button 
-            width={'15%'} 
-            height={'35px'} 
-            onClick={this.handleEnviar}>
-              Enviar
-          </Button>
-
-        </S.WhatsContainer>
-        
-
-      </S.Container>
+          oi
+        </S.WhatsTyping>       
+      </S.WhatsContainer>
     );
   }
 }
--- modulo1/semana5/projeto-whatslab/whatslab/src/style.js
+++ modulo1/semana5/projeto-whatslab/whatslab/src/style.js
@@ -1,41 +1,15 @@
 import styled from "styled-components";
 
-export const WhatsTyping = styled.div`
-    min-height: 90vh;
-    width: 90vh;
-    margin-left: calc(100vh - 45vh);
-    border: 1px solid black;
-`;
-
-export const Container = styled.div`
-    text-align: center;
-    item-align: center:
-    display: ${props => props.display}; 
-`;
-
 export const WhatsContainer = styled.div`
-    width: 90vh;
-    margin-left: calc(100vh - 45vh);
-    height: ${props => props.height};
-`;
-
-
-export const Photo = styled.img`
-    display: block;
-    margin-left: auto;
-    margin-right: auto;
-    border-radius: 50%;
-    margin-top: 5px;
-    margin-bottom: 5px;
-    margin-left: 20px;
-`;
-
-
-export const WhatsUserName = styled.strong`
     display: flex;
-    margin-left: 120px;
+    border: 2px solid black;
+    justify-content: center;
+    align: center;
+    text-align: center;
+    min-height: 100vh;
+    margin: 40px;
 `;
 
-export const WhatsUserNameTyping = styled.span`
-    display: flex;
+export const WhatsTyping = styled.div`
+    display: grid;
 `;
