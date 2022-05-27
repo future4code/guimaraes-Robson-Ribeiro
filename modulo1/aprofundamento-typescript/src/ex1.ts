e7a495e60db4e7ec8c266ec71fe84d6d288f090c
robsonribeiro <robsonribeiro@uffa.com.vc>
2022-05-26 23:14:32 -0300

    exercicio 1

---
+++ modulo1/aprofundamento-typescript/src/ex1.ts
@@ -0,0 +1,18 @@
+
+const minhaString: string = ""
+const meuNumero: number = 0
+
+const Pessoa = {
+    nome: String,
+    idade: Number,
+    corFavorita: String
+}
+
+enum coresArcoIris{
+    VERMELHO = "vermelho",
+    LARANJA = "laranja",
+    VERDE = "verde",
+    AZUL = "azul",
+    ANIL = "anil",
+    ROXO = "roxo"
+}
