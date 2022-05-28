899e9b4a42021ccecf9214174cfead474c248924
robsonribeiro <robsonribeiro@uffa.com.vc>
2022-05-27 22:45:38 -0300

    criado o projeto

---
+++ modulo1/lista-typescript/src/exe1.ts
@@ -0,0 +1,13 @@
+
+type Entrada = {
+    data: string,
+    entrada: string,
+    formatData: (data: string) => string
+}
+
+function recebeString( data: string, entrada: string ) :string {
+
+    return data
+}
+
+console.log(recebeString('27/05/2022','Robson'))
