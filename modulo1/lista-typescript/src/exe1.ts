6d0f11faba8fa5fa41f77193bb91267b219d0d1e
robsonribeiro <robsonribeiro@uffa.com.vc>
2022-05-31 19:14:23 -0300

    exe1

--- modulo1/lista-typescript/src/exe1.ts
+++ modulo1/lista-typescript/src/exe1.ts
@@ -5,9 +5,27 @@ type Entrada = {
     formatData: (data: string) => string
 }
 
+function formatData( data: string ) :string[]{
+
+    return data.split('/')
+}
+
 function recebeString( data: string, entrada: string ) :string {
 
-    return data
+    if(!data){
+        return "Data não foi informada"
+    }
+
+    if(!entrada){
+        return "entrada não foi informada"
+    }
+
+    if(data && entrada){
+        const info: Entrada = {
+            data: data,
+            entrada.formatData(data); 
+        }
+    }
 }
 
 console.log(recebeString('27/05/2022','Robson'))
