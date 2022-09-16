61d2903c9fce83212dd008c58d7614a53b807aae
Robson Rodrigues Ribeiro <projetos.r@hotmail.com>
2022-09-15 21:45:03 -0300

    add tempo de processamento

---
+++ modulo1/complexidade-algoritmos/src/exercicios-1/exec.ts
@@ -0,0 +1,13 @@
+const findFirstRecurringCharacter = (input) => {
+    const charHashMap = {};
+    for (const char of input) {
+      if (charHashMap[char] === true) {
+        return char;
+      }
+      charHashMap[char] = true;
+    }
+    return null;
+  }; 
+ 
+ //Resposta: 6
+ 
